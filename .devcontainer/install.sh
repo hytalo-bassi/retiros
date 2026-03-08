5626bb47800fcc8c4021a48848eec1d2dcf0ccdb
Hytalo Bassi <hytalombassi@gmail.com>
2026-03-08 01:35:18 +0000

    build: melhora devcontainer e adiciona novas dependências ao projeto

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Instalando dependências do PlantUML..."
+echo "Instalando dependências do PlantUML e Cliente PostgreSQL..."
 
 sudo apt-get update -y && sudo apt-get install -y \
   graphviz \
   wget \
   fontconfig \
-  fonts-dejavu-core
+  fonts-dejavu-core \
+  postgresql-client
 
 PLANTUML_VERSION="1.2024.6"
 PLANTUML_JAR="/usr/local/lib/plantuml.jar"
@@ -16,6 +17,8 @@ echo "Baixando PlantUML ${PLANTUML_VERSION}..."
 sudo wget -q \
   "https://github.com/plantuml/plantuml/releases/download/v${PLANTUML_VERSION}/plantuml-${PLANTUML_VERSION}.jar" \
   -O "${PLANTUML_JAR}"
+
+
 
 java -jar "${PLANTUML_JAR}" -version
 
